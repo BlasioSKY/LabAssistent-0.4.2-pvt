9650e138444d27974aeebb60cee96d5fabe41b74
BlasioSKY <158046482+BlasioSKY@users.noreply.github.com>
2024-02-16 19:10:54 +0100

    Update SLY_Lab_Assistant.user.js
    function calculateWarpFuelBurn(fleet, distance) {
        const warpCnt = userFleets[i].maxWarpDistance > 0 ? moveDist / (userFleets[i].maxWarpDistance / 100) : 1;
        return distance * (fleet.warpFuelConsumptionRate / 100) + warpCnt;
    }

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -457,7 +457,9 @@
     }
 
     function calculateWarpFuelBurn(fleet, distance) {
-        return distance * (fleet.warpFuelConsumptionRate / 100)
+    const warpCnt = userFleets[i].maxWarpDistance > 0 ? moveDist / (userFleets[i].maxWarpDistance / 100) : 1;
+    return distance * (fleet.warpFuelConsumptionRate / 100) + warpCnt;
+}
     }
 
     function calculateSubwarpTime(fleet, distance) {
